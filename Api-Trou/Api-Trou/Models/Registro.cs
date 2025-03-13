f7124a5eff3d01b2b4401fe8c91c5f69d85a867a
URIDIOS <xfalokkopax@gmail.com>
2025-03-12 20:18:12 -0700

    Api

---
+++ Api-Trou/Api-Trou/Models/Registro.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Api_Trou.Models
+{
+	public class Registro
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+		public string?  Nombre { get; set; }
+		public string? Correo { get; set; }
+		public string? Contraseña { get; set; }
+	}
+}
---
+++ Api-Trou/Api-Trou/Repositories/MongoDBrepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace Api_Trou.Repositories
+{
+	public class MongoDBrepository
+	{
+		public MongoClient Client;
+		public IMongoDatabase db;
+		public MongoDBrepository()
+		{
+			Client = new MongoClient("Cadena de conección");
+
+			db = Client.GetDatabase("Base de datos");
+		}
+	}
+}
